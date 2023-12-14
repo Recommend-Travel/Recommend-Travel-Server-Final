bbd1184a117c5bc2822689256b0c0f7c1fc1893a
si14444 <a1@1ui-MacBookAir.local>
2023-12-15 03:06:43 +0900

    프로젝트 초기세팅

---
+++ src/main/java/webProgramming/recommendTravel/RecommendTravelApplication.java
@@ -0,0 +1,13 @@
+package webProgramming.recommendTravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendTravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/webProgramming/recommendTravel/RecommendTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package webProgramming.recommendTravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
